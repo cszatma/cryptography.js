a04c8610289a3c1ae7e09ce66eee2b835654abde
Christopher Szatmary <cs@christopherszatmary.com>
2018-06-10 14:50:57 -0400

    Setup the api

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import './utils/stringExtensions';
+import processInput from './processInput';
+import encrypt from './actions/encrypt';
+import decrypt from './actions/decrypt';
+import performSubstitution from './actions/performSubstitution';
+import performTransposition from './actions/performTransposition';
+
+export default {
+  processInput,
+  encrypt,
+  decrypt,
+  performSubstitution,
+  performTransposition,
+};
